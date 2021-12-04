880ef6c2d9234c18bd2ef5eac3e26a88f93edf0d
ymaa <ymaa@ctrip.com>
2021-12-04 23:52:55 +0800

    up

--- src/greedy/index.js
+++ src/greedy/index.js
@@ -1,6 +1,6 @@
 /**
  * id 1518
- * url [ https://leetcode-cn.com/problems/water-bottles/ ]
+ * url [https://leetcode-cn.com/problems/water-bottles/]
  * @param {number} numBottles
  * @param {number} numExchange
  * @return {number}
@@ -17,3 +17,31 @@ var numWaterBottles = function (numBottles, numExchange) {
 	}
 	return drink;
 };
+
+/**
+ * id 1005
+ * url [https://leetcode-cn.com/problems/maximize-sum-of-array-after-k-negations/]
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var largestSumAfterKNegations = function (nums, k) {
+	if (nums.length === 0) return;
+	nums.sort((a, b) => {
+		return a - b;
+	});
+	while (k > 0) {
+		nums[0] = 0 - nums[0];
+		nums.sort((a, b) => {
+			return a - b;
+		});
+		k--;
+	}
+	let sum = 0;
+	nums.forEach((num) => {
+		sum += num;
+	});
+	return sum;
+};
+
+console.log(largestSumAfterKNegations([1, 2, 3, 8, 7, 6], 3));
